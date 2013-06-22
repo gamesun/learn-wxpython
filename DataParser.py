578ab276def917c97e6cef8e301baa8a53e79fee
gamesun <gamesunyt@gmail.com>
2013-06-22 22:07:56 +0800

    modify parser

--- DataParser.py
+++ DataParser.py
@@ -12,40 +12,45 @@ g_match = re.compile('(?P<time>[0-9a-fA-F]{4}) (?P<value>[0-9a-fA-F]{8})')
 def Parser(list):
     """
     parser the data and display results at the canvas.
-    Parser(list) -> [ [duration,]    info
-                      [[x1,y1], [x2,y2], ... [xn,yn]]    wave 1
-                      []    wave 2
+    Parser(list) -> [ (duration),    info
+                      ((x1,y1), (x2,y2), ... (xn,yn)),    wave 1
+                      (...),    wave 2
                       ...
-                      []    wave 32
-                    ]
+                      (...))]    wave 32
     """
     
 #    for l in list:
 #        print l,
-    result = [[0]]
+    result = []
     
     tmp = [g_match.search(l) for l in list ]
     lstData = [t for t in tmp if t is not None]
-
-    for d in lstData:
-        print d.group('time') , d.group('value')
     
     if 0 < len(lstData): 
-        duration = int(lstData[-1].group('time'),16) - int(lstData[0].group('time'),16)
+        duration = int(lstData[-1].group('time'), 16) - int(lstData[0].group('time'), 16)
         result = [[duration]]
 #         print result
-        
-        
-
+        matrix = []
+        for d in lstData:
+#             print d.group('time') , #d.group('value'),
+            matrix.append([(int(d.group('time'), base = 16), int(v)) for v in bits(int(d.group('value'), base = 16), 32)])
+                
+        result.append(zip(*matrix))
+            
     return result
 
-def DrawLine(data):
-    """
+def bits(data):
+    while data:
+        b = data & 0x01
+        yield b
+        data >>= 1
+
+def bits(data, bits):
+    for i in xrange(bits):
+        b = data & 0x01
+        yield b
+        data >>= 1
     
-    """
-    pass
-
-
 def Scale(factor):
     """
     scale the canvas using the factor.
